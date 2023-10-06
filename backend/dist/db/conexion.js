bb58135edf30d76938f9d4c8708e3be56cac5a9d
Edwin Figueroa <edwinprogramador@gmail.com>
2023-10-06 10:44:04 -0600

    Subiendo prueba devtest 1

--- backend/dist/db/conexion.js
+++ backend/dist/db/conexion.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-const db = new sequelize_1.Sequelize('devtest', 'edwintest', '12345', {
+const db = new sequelize_1.Sequelize('devtest', 'user', 'password', {
     host: 'localhost',
     dialect: 'mysql'
 });
